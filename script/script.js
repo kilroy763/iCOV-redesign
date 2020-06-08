41f9ec3a842133aadd3a89c4380f79123c929078
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2020-06-08 15:24:12 +0200

    mobiele variant

--- script/script.js
+++ script/script.js
@@ -1,3 +1,129 @@
+
+// Code to toggle the hamburger menu ------------------------
+const menuBtn = document.querySelector('.nav_toggle');
+const menu = document.querySelector('.menu');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menu.classList.add('active');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menu.classList.remove('active');
+    menuOpen = false;
+  }
+});
+
+//---------------------------------------------------------
+
+
+// code to toggle the line under NL and ENG ---------------
+
+var el = {
+  one: document.querySelector('.nl'),
+  two: document.querySelector('.eng'), 
+  container: document.querySelector('.language_toggle')
+};
+
+var func = {
+  toggleActive: function(ele) {
+    ele = event.target;
+    var hold = el.container.children,
+        huh = el.container.children.length,
+        hasActive = ele.classList.contains('toggled');
+    for (i = 0; i < huh; i++) {
+      if (hold[i].classList.contains('toggled')) {
+        hold[i].classList.remove('toggled');
+      }
+    }
+    if (!hasActive) {
+      ele.classList.add('toggled');
+    }
+  }
+};
+
+el.one.addEventListener("click", func.toggleActive);
+el.two.addEventListener("click", func.toggleActive);
+
+//-------------------------------------------------------- 
+
+ 
+// Code to show the audio player within an overlay at the bottom of a viewport
+document.querySelector(".removeoverlay").addEventListener("click", stopaudio);
+
+document.querySelector(".showaudio").addEventListener("click", myFunction);
+
+function myFunction() {
+    var element = document.querySelector(".removeaudioplayer");
+    element.classList.add("show");
+    audio.play();
+    isPlaying = true;
+    playSound();
+    console.log("het werkt")
+}
+//------------------------------------------------------ 
+
+
+//code to stop the podcast with custom buttom
+function stopaudio() {
+    var element = document.querySelector(".removeaudioplayer");
+    element.classList.remove("show");
+    isPlaying = false;
+    playSound();
+    console.log("het werkt")
+}
+//------------------------------------------------------ 
+
+//code to play the podcast with custom button
+function playSound() {
+    var audioPlayer = document.getElementById(audio);
+    if (audioPlayer.pause()) {
+        audioPlayer.play();
+    } else {
+        audioPlayer.pause();
+    }
+}
+
+//------------------------------------------------------ 
+
+
+const acc = document.getElementsByClassName("accordion");
+
+ for (let i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function(i) {
+  const panel = document.getElementsByClassName("panel");
+  console.log(panel)
+  panel[i].classList.add("panelOn")
+ })
+}
+
+// Commented code, WIP -----------------------------------
+
+//  function klap(a) {
+//   console.log("hallo")
+//   panel[a].classList.toggle("panelOn")
+//  }
+
+// let acc = document.getElementsByClassName("accordion");
+// let panel = document.getElementsByClassName("panel");
+// let i;
+
+//  for (i = 0; i < acc.length; i++) {
+//  acc[i].addEventListener("click", function() {
+//     console.log('klikie')
+//     // const panelt = document.querySelector(".panel");
+//     let panelState;
+//     if (panelState == true) {
+//       panel[i].classList.remove("panelOn");
+//       panelState = false;
+//     } else {
+//       panel[i].classList.add("panelOn");
+//       panelState = true;
+//     }
+//   });
+// }
+
 // =================================================================================================
 
 // All buttons in een keer:
@@ -37,90 +163,3 @@
 //     translation = '-33%';
 //   }
 // }
-
-// =================================================================================================
-
-
-
-
-const menuBtn = document.querySelector('.nav_toggle');
-const menu = document.querySelector('.menu');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    menu.classList.add('active');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    menu.classList.remove('active');
-    menuOpen = false;
-  }
-});
-
- 
-
-document.querySelector(".wegaudio").addEventListener("click", stopaudio);
-
-document.querySelector(".hoiaudio").addEventListener("click", myFunction);
-
-function myFunction() {
-    var element = document.querySelector(".wegaudioplayer");
-    element.classList.add("latenzien");
-    //var audio = document.getElementById("audio")
-    audio.play();
-    isPlaying = true;
-    playSound();
-    console.log("het werkt")
-}
-
-function stopaudio() {
-    var element = document.querySelector(".wegaudioplayer");
-    element.classList.remove("latenzien");
-    isPlaying = false;
-    playSound();
-    console.log("het werkt")
-}
-
-function playSound() {
-    var audioPlayer = document.getElementById(audio);
-    if (audioPlayer.pause()) {
-        audioPlayer.play();
-    } else {
-        audioPlayer.pause();
-    }
-}
-
-// let acc = document.getElementsByClassName("accordion");
-// let panel = document.getElementsByClassName("panel");
-// let i;
-
-//  for (i = 0; i < acc.length; i++) {
-//  acc[i].addEventListener("click", function() {
-//     console.log('klikie')
-//     // const panelt = document.querySelector(".panel");
-//     let panelState;
-//     if (panelState == true) {
-//       panel[i].classList.remove("panelOn");
-//       panelState = false;
-//     } else {
-//       panel[i].classList.add("panelOn");
-//       panelState = true;
-//     }
-//   });
-// }
-
-const acc = document.getElementsByClassName("accordion");
-
- for (let i = 0; i < acc.length; i++) {
- acc[i].addEventListener("click", function(i) {
-  const panel = document.getElementsByClassName("panel");
-  console.log(panel)
-  panel[i].classList.add("panelOn")
- })
-}
-
-//  function klap(a) {
-//   console.log("hallo")
-//   panel[a].classList.toggle("panelOn")
-//  }
