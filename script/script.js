bf8d457ccd6683e340366803d9316e5857e596d0
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2020-06-11 15:30:19 +0200

    x
    x

--- script/script.js
+++ script/script.js
@@ -1,27 +1,6 @@
 
 // Code to toggle the hamburger menu ------------------------
 
-$(function(){
-  var overlay = $('<div id="overlay"></div>');
-  overlay.show();
-  overlay.appendTo(document.body);
-  $('.popup').show();
-  $('.close').click(function(){
-  $('.popup').hide();
-  overlay.appendTo(document.body).remove();
-  return false;
-  });
-  
-  
-   
-  
-  $('.x').click(function(){
-  $('.popup').hide();
-  overlay.appendTo(document.body).remove();
-  return false;
-  });
-  });
-  
  const menuBtn = document.querySelector('.nav_toggle');
  const menu = document.querySelector('.menu');
  let menuOpen = false;
