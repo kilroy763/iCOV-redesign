6b977b539ad2249c7be643f4552c5feb18fde652
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2020-06-12 11:13:50 +0200

    x

--- script/script.js
+++ script/script.js
@@ -47,6 +47,49 @@
 //   }
 // };
 
+$(function(){
+  var overlay = $('<div id="overlay"></div>');
+  overlay.show();
+  overlay.appendTo(document.body);
+  $('.popup').show();
+  $('.close').click(function(){
+  $('.popup').hide();
+  overlay.appendTo(document.body).remove();
+  return false;
+  });
+  
+  
+   
+  
+  $('.x').click(function(){
+  $('.popup').hide();
+  overlay.appendTo(document.body).remove();
+  return false;
+  });
+  });
+
+ 
+
+
+  
+  var swiper3 = new Swiper('.swiper-container', {
+    centeredSlides: true,
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+      breakpoints: {
+    // when window width is >= 320px
+    1500: {
+
+			noSwiping: true,
+			allowSlidePrev: false,
+			allowSlideNext: false
+    },
+  }});
+
+
+
 // el.one.addEventListener("click", func.toggleActive);
 // el.two.addEventListener("click", func.toggleActive);
 
@@ -109,30 +152,30 @@
 //   else $('.swiper-scrollbar').show()
 // })
 
-var swiper = new Swiper(".swiper", {
-  slidesPerView: 3,
-  spaceBetween: 50,
-  grabCursor: true,
-  navigation: {
-    nextEl: '.button_topic_right'
-  },
-});
+// var swiper = new Swiper(".swiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 50,
+//   grabCursor: true,
+//   navigation: {
+//     nextEl: '.button_topic_right'
+//   },
+// });
 
-var swiper2 = new Swiper(".swiper-container", {
-  slidesPerView: 'auto',
-  spaceBetween: 30,
-  grabCursor: true,
-  navigation: {
-    nextEl: '.button_podcast_right'
-  },
-  breakpoints: {
-    // when window width is >= 320px
-    1500: {
-      allowSlidePrev: false,
-      allowSlideNext: false,
-      allowTouchMove: false
-    },
-}});
+// var swiper2 = new Swiper(".swiper-container", {
+//   slidesPerView: 'auto',
+//   spaceBetween: 30,
+//   grabCursor: true,
+//   navigation: {
+//     nextEl: '.button_podcast_right'
+//   },
+//   breakpoints: {
+//     // when window width is >= 320px
+//     1500: {
+//       allowSlidePrev: false,
+//       allowSlideNext: false,
+//       allowTouchMove: false
+//     },
+// }});
 
 //  function klap(a) {
 //   console.log("hallo")
@@ -215,19 +258,79 @@ var swiper2 = new Swiper(".swiper-container", {
  */
 
 
- /*
+ 
 
- var observer = new IntersectionObserver(onIntersect, {
-   threshold: 0.6
- });
+//  var observer = new IntersectionObserver(onIntersect, {
+//    threshold: 0.3
+//  });
 
- var sections = document.querySelectorAll('.year-section');
+//  var sections = document.querySelectorAll('.year-section');
 
- sections.forEach(section => observer.observe(section));
+//  sections.forEach(section => observer.observe(section));
 
- function onIntersect(entries) {
-   var element = entries[0];
-   var year = element.dataset.year;
- }
+//  function onIntersect(entries) {
+//    var element = entries[1];
+//    var year = element.dataset.year;
+//    console.log("werkt")
+//  }
 
- */
+
+// var intersectionObserver = new IntersectionObserver(function(entries) {
+//   // If intersectionRatio is 0, the target is out of view
+//   // and we do not need to do anything.
+//   if (entries[0].intersectionRatio <= 0) return;
+
+//   loadItems(10);
+//   console.log('Loaded new items');
+// });
+// // start observing
+// intersectionObserver.observe(document.querySelector('.scrollerFooter'));
+
+//
+
+const header = document.querySelector(".werkend");
+const sectionOne = document.querySelector("#one");
+
+var thresholds = IntersectionObserver.thresholds;
+console.log(thresholds)
+
+const sectionOneOptions = {
+  threshold: 0.5
+};
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach(entry => {
+    if (!entry.intersectionRatio > 0.5) {
+      header.classList.add("werkt");
+      console.log('werkt')
+    } else {
+      header.classList.remove("werkt");
+      console.log('weg')
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+// const blok = document.querySelectorAll('.year-section');
+
+// observer = new IntersectionObserver((entries) =>{
+// entries.forEach(entry =>{
+//   if(entry.intersectionRatio > 0.5){
+//     blok.classList.add("werkt");
+//     console.log('werkt')
+//   }
+//   else {
+//     blok.classList.remove("werkt");
+//     console.log('weg')
+//   }
+// })
+// })
+
+// blok.forEach(blokje => {
+//   observer.observe(blokje)
+// })
