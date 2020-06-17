9e9a22b0eb9667d00efa2fdaf7065daf6292dde5
kilroy763 <58260542+kilroy763@users.noreply.github.com>
2020-06-17 17:31:48 +0200

    timeline fixed

--- script/timeline.js
+++ script/timeline.js
@@ -1,10 +1,12 @@
 $(function(){
-    var overlay = $('<div id="overlay"></div>');
+    var overlay = $('<div class="overlay"></div>');
     overlay.show();
     overlay.appendTo(document.body);
     $('.popup').show();
+
     $('.close').click(function(){
     $('.popup').hide();
+
     overlay.appendTo(document.body).remove();
     return false;
     });
